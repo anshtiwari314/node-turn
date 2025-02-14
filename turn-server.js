7b6b4c26723610f99740ef4d2a774673eace993c
anshtiwari314 <anshtiwari314@gmail.com>
2025-02-15 02:07:32 +0530

    modified options

--- turn-server.js
+++ turn-server.js
@@ -3,18 +3,22 @@ let dotenv = require('dotenv');
 const express = require('express')
 let app = express()
 dotenv.config()
-let PORT = process.env.PORT || 5010
-let HOST = process.env.HOST || '127.0.0.1'
-console.log("the current port and host",PORT,HOST)
 
-app.set('host', HOST);
-console.log()
+let PORT = process.env.PORT || 3478
+let HOST = process.env.HOST || '127.0.0.1'
+
+console.log("the current port and host",PORT)
+
+//app.set('host', '0.');
+
+
 var server = new Turn({
   // set options
   listeningPort:PORT,
+  listeningIps: ['0.0.0.0'],
   authMech: 'long-term',
   credentials: {
     anshtiwari314: "jokanomy-jarvis"
   }
 });
-server.start();
+server.start(()=>console.log(`TURN server is running on port ${PORT}`));
